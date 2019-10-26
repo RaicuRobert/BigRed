3ab4b4250964b7ce1a5a654c591b600f145f9c36
Raicu Robert <raicurobert39@gmail.com>
2019-10-26 12:36:31 +0300

    Refactored the scanning view

--- tesco_share/lib/main.dart
+++ tesco_share/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tesco_share/Vision/Vision.dart';
+import 'package:tesco_share/ProductScan/ScannerView.dart';
 import 'package:tesco_share/pages/CategoryList.dart';
 
 import 'Colors.dart';
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return CategoryList();
+    return ScannerView();
 
 
 
