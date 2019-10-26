596f64af7883fd8436b94b9df1af617d99714231
Raicu Robert <raicurobert39@gmail.com>
2019-10-26 10:28:43 +0300

    OCR

--- tesco_share/lib/main.dart
+++ tesco_share/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tesco_share/Vision/Vision.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(VisionApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
